d0c0e6a5498095108bc71474c8159d490f0bd8dc
Dimitris Kranias <50874678+dimitriskranias@users.noreply.github.com>
2021-02-21 02:42:05 +0200

    Update rdd_q2.py

--- rdd_q2.py
+++ rdd_q2.py
@@ -7,15 +7,9 @@ sc = spark.sparkContext
 hdfs = "hdfs://master:9000/data/"
 
 result = sc.textFile(hdfs + "ratings.csv"). \
-<<<<<<< HEAD
 	 map(lambda x : (x.split(',')[0], (float(x.split(',')[2]), 1))). \
 	 reduceByKey(lambda x,y : (x[0] + y[0], x[1] + y[1])). \
 	 map(lambda x : (x[0], x[1][0] / x[1][1]))
-=======
-         map(lambda x : (x.split(',')[0], (float(x.split(',')[2]), 1))). \
-         reduceByKey(lambda x,y : (x[0] + y[0] , x[1] + y[1])). \
-         map(lambda x : (x[0], x[1][0] / x[1][1]))
->>>>>>> 84cca547eda01d38e99740a3b065c05208c9b627
 
 cnt_initial = result.count()
 
@@ -29,8 +23,4 @@ for x in range(100):
 print(str(100 * (cnt_final/cnt_initial)) + "%")
 
 for x in range(100):
-<<<<<<< HEAD
 	print('RESULT')
-=======
-        print('RESULT')
->>>>>>> 84cca547eda01d38e99740a3b065c05208c9b627
